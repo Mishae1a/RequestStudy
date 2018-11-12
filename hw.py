5716e2db276a1ab8c8e2295159e0de4f56e71c7a
Shi Hongda <shihongda@zsfucai.cn>
2018-11-12 17:58:17 +0800

    [master]

--- hw.py
+++ hw.py
@@ -1,4 +1,4 @@
 import requests
-r = requests.get('https://www.douban.com/')
+r = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json')
 print(r.status_code)
-print(r.text)
+print(r.json())
