2cbcc81d346f31877d02c21aee8300022be68c06
shihongda <shihongda@zsfucai.cn>
2019-12-12 15:07:13 +0800

    [master]

--- ljSpider/controller/runList.py
+++ ljSpider/controller/runList.py
@@ -10,39 +10,39 @@ def getListData(page):
     cursor = conn.cursor(dictionary=True)
     pg = 1
     while 1:
-        try:
-            lianjiaObj = LianjiaList.LianjiaList()
-            houseList = lianjiaObj.getUrlData(page + 'pg' + str(pg))
-            # print(page + 'pg' + str(pg))
-            if (houseList['code'] != 0):
-                print(page + ' 页面请求失败' + str(houseList['code']) + houseList['msg'])
-                return
-            for house in houseList['data']:
-                # Read a single record
-                sql = "select * from lj_transaction WHERE lj_id = %s LIMIT 1"
-                print(house)
-                cursor.execute(sql, (house['lj_id'],))
-                result = cursor.fetchone()
-                if (result == None):
-                    cursor.execute("INSERT INTO lj_transaction (lj_id, title, trade_time, total_price, total_unit, unit_price, unit_unit, status) "
-                            + " VALUES (%(lj_id)s, %(title)s, %(trade_time)s, %(total_price)s, %(total_unit)s, %(unit_price)s, %(unit_unit)s, %(status)s)",
-                            house
-                        )
-                    conn.commit()
-                else:
-                    # 更新原有的
-                    # cursor.execute("UPDATE lj_transaction SET title = %(title)s, trade_time = %(trade_time)s, total_price = %(total_price)s," 
-                    #     +" total_unit = %(total_unit)s, unit_price = %(unit_price)s, unit_unit = %(unit_unit)s, status = %(status)s, keyword = %(keyword)s WHERE lj_id=%(lj_id)s", house)
-                    # conn.commit()
-                    # 列表页似乎也不用更新
-                    pass
-            # 如果本页数据不为空 继续抓下一页
-            if (len(houseList['data']) == 0):
-                break
+        # try:
+        lianjiaObj = LianjiaList.LianjiaList()
+        houseList = lianjiaObj.getUrlData(page + 'pg' + str(pg))
+        # print(page + 'pg' + str(pg))
+        if (houseList['code'] != 0):
+            print(page + ' 页面请求失败' + str(houseList['code']) + houseList['msg'])
+            return
+        for house in houseList['data']:
+            # Read a single record
+            sql = "select * from lj_transaction WHERE lj_id = %s LIMIT 1"
+            print(house)
+            cursor.execute(sql, (house['lj_id'],))
+            result = cursor.fetchone()
+            if (result == None):
+                cursor.execute("INSERT INTO lj_transaction (lj_id, title, trade_time, total_price, total_unit, unit_price, unit_unit, status) "
+                        + " VALUES (%(lj_id)s, %(title)s, %(trade_time)s, %(total_price)s, %(total_unit)s, %(unit_price)s, %(unit_unit)s, %(status)s)",
+                        house
+                    )
+                conn.commit()
             else:
-                pg += 1
-        except BaseException as e:
-            print('[error] [%s] [%s]' % (page, e))
+                # 更新原有的
+                # cursor.execute("UPDATE lj_transaction SET title = %(title)s, trade_time = %(trade_time)s, total_price = %(total_price)s," 
+                #     +" total_unit = %(total_unit)s, unit_price = %(unit_price)s, unit_unit = %(unit_unit)s, status = %(status)s, keyword = %(keyword)s WHERE lj_id=%(lj_id)s", house)
+                # conn.commit()
+                # 列表页似乎也不用更新
+                pass
+        # 如果本页数据不为空 继续抓下一页
+        if (len(houseList['data']) == 0):
+            break
+        else:
+            pg += 1
+        # except BaseException as e:
+        #     print('[error] [%s] [%s]' % (page, e))
 
     cursor.close()
     conn.close()
